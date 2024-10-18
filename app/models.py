020f09b91eaf837a5ea45421e52db68c046d4415
tomutanyi <tomutanyi@gmail.com>
2024-10-18 09:51:58 +0300

    seeded database successfully

--- app/models.py
+++ app/models.py
@@ -6,10 +6,10 @@ db = SQLAlchemy()
 class Client(db.Model, SerializerMixin):
     __tablename__ = 'clients'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
-    address = db.Column(db.String(200), nullable=True)
+    phone_number = db.Column(db.String(40), nullable=False)
+    address = db.Column(db.String(300), nullable=True)
 
     def __repr__(self):
         return f'<Client {self.name}>'
