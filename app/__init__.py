747db57e32b342da0c423436fe3639f6cce37c9e
tomutanyi <tomutanyi@gmail.com>
2024-10-21 12:43:52 +0300

    moved CORS from init to app

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_restx import Api
-from flask_cors import CORS
+# from flask_cors import CORS
 from .config import Config
 from .models import Client, db
 
@@ -11,6 +11,7 @@ jwt = JWTManager()
 bcrypt = Bcrypt()
 api = Api()
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
@@ -21,8 +22,6 @@ def create_app():
     bcrypt.init_app(app)
     api.init_app(app)
     migrate.init_app(app, db)
-
-    CORS(app)  # Apply CORS to the app
 
     from .routes import client_ns, device_ns, users_ns
     api.add_namespace(client_ns)
