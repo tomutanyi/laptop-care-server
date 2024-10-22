ed5514b5192c3207a75ed6cbb0a10e54d72e454a
tomutanyi <tomutanyi@gmail.com>
2024-10-22 13:03:37 +0300

    changed endpoint from user_login to login

--- app/routes.py
+++ app/routes.py
@@ -198,7 +198,7 @@ class UserResource(Resource):
         return 'Deleted User', 204
     
 
-@users_ns.route('/login', endpoint='user_login')
+@users_ns.route('/login', endpoint='login')
 class UserLoginResource(Resource):
     def post(self):
         """Authenticate user and return a JWT."""
